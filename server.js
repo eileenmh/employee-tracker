9bb9e8c609a82f88139c863e068178fa5b8f4b1e
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-19 21:22:45 -0400

    require express and mysql2

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "f@7vgtoDpMpw@RJ",
+  database: "",
+});
