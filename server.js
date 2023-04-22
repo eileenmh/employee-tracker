a94f7d6fc3f25a576015a7c9cfbcdcdb82f9a569
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-22 12:02:55 -0400

    change inquirer version and create options question

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 /* cSpell:disable */
 
-const express = require("express");
+const inquirer = require("inquirer");
+inquirer.registerPrompt("search-list", require("inquirer-search-list"));
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "f@7vgtoDpMpw@RJ",
-  database: "",
-});
+// const db = createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "f@7vgtoDpMpw@RJ",
+//   database: "",
+// });
+
+function selectQuery() {
+  inquirer
+    .prompt({
+      type: "search-list",
+      message: "Welcome to the Employee Manager. What would you like to do?",
+      name: "selection",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+      ],
+    })
+    .then(function (answers) {
+      console.log(JSON.stringify(answers, null, "  "));
+    })
+    .catch((e) => console.log(e));
+}
+
+selectQuery();
