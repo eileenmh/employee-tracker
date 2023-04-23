97b1b4a678d35bdf9b43ffb39561eb3a2818d9ba
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-04-22 20:12:29 -0400

    set up sql query with Promise

--- queries.js
+++ queries.js
@@ -1,21 +1,44 @@
-const mysql = require("mysql2");
-const cTable = require("console.table");
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "f@7vgtoDpMpw@RJ",
-  database: "employee_tracker",
-});
-
 function Query() {}
 
-Query.viewDepartments = function () {
-  db.query("SELECT * FROM department", function (err, results) {
-    console.table(results);
+Query.viewDepartments = async function () {
+  // get the client
+  let mysql = require("mysql2/promise");
+  // create the connection
+  let connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "f@7vgtoDpMpw@RJ",
+    database: "employee_tracker",
   });
+  let result = await connection.query("SELECT * FROM department");
+  console.table(result[0]);
 };
 
-Query.viewRoles = [];
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "f@7vgtoDpMpw@RJ",
+//   database: "employee_tracker",
+// });
+
+//
+
+// Query.viewDepartments = function () {
+//   db.query("SELECT * FROM department", function (err, results) {
+//     console.table(results);
+//   });
+// };
+
+// Query.viewRoles = function () {
+//   db.query("SELECT * FROM role", function (err, results) {
+//     console.table(results);
+//   });
+// };
+
+// Query.viewEmployees = function () {
+//   db.query("SELECT * FROM employee", function (err, results) {
+//     console.table(results);
+//   });
+// };
 
 module.exports = Query;
--- server.js
+++ server.js
@@ -2,35 +2,61 @@
 
 const inquirer = require("inquirer");
 inquirer.registerPrompt("search-list", require("inquirer-search-list"));
-const mysql = require("mysql2");
 
-// const db = createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "f@7vgtoDpMpw@RJ",
-//   database: "",
-// });
+const query = require("./queries");
 
 function selectQuery() {
-  inquirer
-    .prompt({
-      type: "search-list",
-      message: "Welcome to the Employee Manager. What would you like to do?",
-      name: "selection",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role",
-      ],
-    })
-    .then(function (answers) {
-      console.log(JSON.stringify(answers, null, "  "));
-    })
-    .catch((e) => console.log(e));
+  return inquirer.prompt({
+    type: "search-list",
+    message: "What would you like to do?",
+    name: "selection",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+    ],
+  });
 }
 
-selectQuery();
+const processQuery = async function (response) {
+  switch (response.selection) {
+    case "View all departments":
+      await query.viewDepartments();
+      return true;
+    case "View all roles":
+      break;
+    case "View all employees":
+      break;
+    case "Add a department":
+      break;
+    case "Add a role":
+      break;
+    case "Add an employee":
+      break;
+    case "Update an employee role":
+  }
+};
+
+function restartPrompt() {
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "restart",
+      message: "Query complete. Would you like to do something else?",
+    })
+    .then((response) => {
+      if (response.restart === true) {
+        init();
+      }
+    });
+}
+
+function init() {
+  selectQuery().then(processQuery).then(restartPrompt);
+}
+
+init();
